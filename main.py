700fde83ae212953105de778919c57b39813e4ae
Kaushik <panchalkaushik028@gmail.com>
2023-09-01 19:06:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Taking two inputs from the user
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+# 1. Floor Division
+floor_division_result = a // b
+print("Floor Division:", floor_division_result)
+
+
+# 2. a power b
+power_result = a ** b
+print("a power b:", power_result)
+
+
+# 3. Check if two variables have equal value
+# equal_value = a == b
+if (a == b):
+    print("True")
+else:
+    print("False")
+
+
+# Printing the results
